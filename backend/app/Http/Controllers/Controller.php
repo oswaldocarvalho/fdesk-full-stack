fe30144b0bf7631999b501df2a5a5e5aeaf11809
Oswaldo de Carvalho Filho <oswaldo.vix@gmail.com>
2021-08-06 23:58:43 -0300

    ajustes estruturais

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
@@ -12,28 +14,46 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     /**
+     * Resposta padronizada para todos os requests da api
+     *
      * @param string $message
      * @param int $status
      * @param string|null $token
      * @param null $data
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function jsonResonse(string $message, int $status=200, string $token=null, $data = null): \Illuminate\Http\JsonResponse
+    public function jsonResonse(string $message, int $status=200, string $token=null, $data = null): JsonResponse
     {
-        $response = [
+        $responseData = [
             "message" => $message
         ];
 
-        if ($data)
-        {
-            $response["data"] = $data;
-        }
-
         if ($token)
         {
-            $response["token"] = $token;
+            $responseData["token"] = $token;
         }
 
-        return response()->json($response, $status);
+        if ($data)
+        {
+            $responseData["data"] = $data;
+        }
+
+        return response()->json($responseData, $status);
+    }
+
+    /**
+     * Valida o request e retorna apenas os campos especificados na validação
+     *
+     * @param Request $request
+     * @param array $rules
+     * @return array
+     */
+    public function validate(Request $request, array $rules):object
+    {
+        //
+        $request->validate($rules);
+
+        //
+        return (object)$request->only(array_keys($rules));
     }
 }
