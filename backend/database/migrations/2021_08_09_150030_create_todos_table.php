59384aed49f35e2087562ea7e9438f262dd6628a
Oswaldo de Carvalho Filho <oswaldo.vix@gmail.com>
2021-08-09 16:12:17 -0300

    Migration (relativa ao commit anterior)

---
+++ backend/database/migrations/2021_08_09_150030_create_todos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('todo');
+            $table->timestamp('completed_at')->nullable(true);
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('todos');
+    }
+}
